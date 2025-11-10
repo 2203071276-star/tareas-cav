74f29b19106692c471a9fb22e2a8454cc72462ee
2203071276-star <2203071276@correo.ler.uam.mx>
2025-11-09 20:37:55 -0500

    Add files via upload
    En mi repositorio de GitHub voy subir las dos carpetas y el archivo PDF, dejando  que corresponden a un único proyecto. Explico profesor hernadi que una carpeta contiene los trabajos realizados con p5.js en el Web Editor, mientras que la otra incluye los desarrollados con p5.js en Visual Studio Code (VSC).  Saludos cordiales

---
+++ P5_Aaron_Vilchis_G._8nov2025_2025_11_09_01_24_35/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(20, 200, 140);
+  stroke ('blue');
+  strokeWeight (4);
+  line (60, 80, 200, 175);
+  line (100, 300, 50, 150);
+  stroke ('red');
+  strokeWeight (4);
+  fill (25, 30, 180);
+  ellipse (50, 50, 80, 40);
+  fill (25, 60, 30)
+  ellipse (400, 400, 120, 60);
+  stroke (20,200, 140);
+  fill (55, 120, 150);
+  ellipse (250, 45, 120, 80);
+  fill (75, 120, 60);
+  ellipse (440, 250, 80, 130);
+  fill (180, 160, 150)
+  rect (180, 400, 130, 60)
+  stroke ('green');
+  strokeWeight (3);
+  fill (200, 200, 190);
+  rect (400, 50, 55, 40);
+  fill (35, 40, 62);
+  rect (50, 400, 70, 80, 20);
+  stroke ('yellow')
+  strokeWeight (5)
+  fill (60, 120, 110);
+  rect (250, 180, 100, 130);
+   
+}
